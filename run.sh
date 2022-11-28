75c58166b2082443510c73f91912cd64a2281170
John Cairns <john@2ad.com>
2022-11-28 15:09:38 -0600

    cross compilation currently does not work

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ VERSION=$(git rev-parse HEAD | cut -c 1-8)
 PROJECT=collectivexyz/foundry
 
 DOCKER_BUILDKIT=1 docker build --progress plain . -t ${PROJECT}:${VERSION} && \
-	docker run --rm -i -t ${PROJECT}:${VERSION}
+	docker run --rm -i -t ${PROJECT}:${VERSION} echo "Foundry RS"
